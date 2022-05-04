f329d4c280875fd2c99134f482618059e4518111
Paul Neville <pauljneville@live.com>
2022-05-04 15:55:10 +1000

    created and configured firebase app and firestore database with mock user data

---
+++ lib/firebase.ts
@@ -0,0 +1,66 @@
+
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { getFirestore, addDoc, setDoc, doc, collection, onSnapshot, getDoc, getDocs, writeBatch, query, where, limit, Timestamp, serverTimestamp } from 'firebase/firestore'
+import { getStorage, TaskEvent } from 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA84gQhLuqwTAtE8SCG-ZSN0Ll38r3iGk4",
+    authDomain: "for-real-bug-tracker.firebaseapp.com",
+    projectId: "for-real-bug-tracker",
+    storageBucket: "for-real-bug-tracker.appspot.com",
+    messagingSenderId: "700262348843",
+    appId: "1:700262348843:web:d7cd37f7a48fae8df9126e",
+    measurementId: "G-F6HKHYVN4Q"
+};
+
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const googleAuthProvider = new GoogleAuthProvider();
+export const googleSignInWithPopup = signInWithPopup;
+export const googleSignOut = signOut;
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
+
+/**
+ * Gets a users/{uid} document by username
+ * @param  {string} username
+ */
+export async function getUserWithUsername(username) {
+    const usersRef = collection(firestore, 'users');
+    const q = query(usersRef, where('username', '==', username), limit(1));
+    const snapshot = await getDocs(q);
+    const userDoc = snapshot.docs[0];
+    return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+    const data = doc.data();
+    return {
+        ...data,
+        // firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+        createdAt: data.createdAt.toMillis(),
+        updatedAt: data.updatedAt.toMillis(),
+    };
+}
+
+/**
+ * Gets all users
+ */
+export async function getAllUsers() {
+    const usersRef = collection(firestore, 'users');
+    const q = query(usersRef);
+    const snapshot = await getDocs(q);
+    return snapshot.docs;
+}
+
+export const fromMillis = Timestamp.fromMillis;
+export const timestampOfServer = serverTimestamp();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Metatags from '../components/Metatags'
+import { getAllUsers } from '../lib/firebase'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const allUserDocs = await getAllUsers();
+
+  if (!allUserDocs) {
+    return { noUsers: true, };
+  }
+
+  const users = allUserDocs.map((userDoc) => {
+    const data = userDoc.data();
+    console.log(data);
+    return { ...data };
+  })
+
+  // const users = JSON.stringify(usersData);
+
+  return {
+    props: { users },
+  }
+}
+
+export default function Home({ users }) {
   return (
     <div className={styles.container}>
       <Metatags />
@@ -17,6 +38,13 @@ export default function Home() {
           This is a web app to track tasks and issues in a software development project and to assign users who are governed by user roles that determine their access to functions.
         </p>
         <img src="https://bug-tracker-one.vercel.app/v0/page-project-with-tickets.png" alt="possible future version of said tracking app" />
+
+        <p>We have the following users registered:</p>
+        <ul>
+          {users.map((user) => {
+            <li>{user.firstName}</li>
+          })}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
