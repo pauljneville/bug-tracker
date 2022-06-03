d5cd19eb62a95ed76efe2b39c1e3aeb3a750a032
Paul Neville <pauljneville@live.com>
2022-06-03 17:08:38 +1000

    added new project form

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -5,6 +5,9 @@ import { useContext, useEffect, useState, useCallback } from 'react';
 import { UserContext } from '../../lib/context';
 import { firestore, auth, googleAuthProvider, googleSignInWithPopup, googleSignOut } from '../../lib/firebase';
 import { getFirestore, addDoc, setDoc, doc, collection, onSnapshot, getDoc, getDocs, writeBatch, query, where, limit, Timestamp, serverTimestamp } from 'firebase/firestore'
+import debounce from 'lodash.debounce';
+// TODO change styles
+import styles from '../../styles/EnterPage.module.css';
 
 export default function Projects() {
     const { user, username } = useContext(UserContext);
@@ -60,7 +63,137 @@ export default function Projects() {
                         );
                     })}
                 </ul>
+
+                <h2>Add New Project</h2>
+                <NewProjectForm />
             </main>
+        </>
+    );
+}
+
+const NewProjectForm = () => {
+    const [projectDescription, setProjectDescription] = useState('');
+    // TODO check uniqueness of project code
+    const [projectCode, setProjectCode] = useState('');
+
+    // TODO check uniqueness of project name
+    const [projectName, setProjectName] = useState('');
+    const [isValid, setIsValid] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const { user, username } = useContext(UserContext);
+
+    // TODO check uniqueness of said project names, keep list and create set?
+    // useEffect(() => {
+    //     checkProjectName(projectName);
+    // }, [projectName]);
+
+    const onProjectNameChange = (e) => {
+        // username must be between 3 and 15 characters long
+        // containing only letters, numbers, underscore
+        const val = e.target.value.toLowerCase();
+        const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+        // check that username is >= 3 characters and passes regex format
+        if (val.length < 3) {
+            setProjectName(val);
+            setLoading(false);
+            setIsValid(false);
+        }
+        if (re.test(val)) {
+            setProjectName(val);
+            setLoading(true);
+            setIsValid(false);
+        }
+    };
+
+    // TODO change regex to accept hyphen and numbers
+    const onProjectCodeChange = (e) => {
+        setProjectCode(e.target.value);
+    };
+
+    const onProjectDescriptionChange = (e) => {
+        setProjectDescription(e.target.value);
+    };
+
+    // const checkProjectName = useCallback(debounce(async (username) => {
+    //     if (username.length >= 3) {
+    //         const ref = doc(firestore, `usernames/${username}`);
+    //         const documentRef = await getDoc(ref);
+    //         setIsValid(documentRef.exists() == false);
+    //         setLoading(false);
+    //     }
+    // }, 500), []
+    // );
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        // get/create the documents that will change in this operation
+        const userDoc = doc(firestore, `projects/${projectCode}`);
+        // TODO add project code to projects list
+        // const usernameDoc = doc(firestore, `usernames/${projectName}`);
+
+        const batch = writeBatch(firestore);
+        try {
+            // batch set the changes to the documents
+            batch.set(
+                userDoc,
+                {
+                    code: projectCode,
+                    description: projectDescription,
+                    name: projectName,
+                    // TODO add edit with users list to add owner
+                    owner: "john-cambell",
+                    // TODO add drop down for list of stages
+                    stage: "planning",
+                    ticketCounts: {
+                        closed: 0,
+                        holding: 0,
+                        pending: 0,
+                        total: 0,
+                        working: 0,
+                    },
+                    version: "0.0.1",
+                }
+            );
+            // TODO add project code to projects list
+            // batch.set(usernameDoc, { uid: user.uid });
+            await batch.commit();
+        } catch (error) {
+            console.error("error with user batch write");
+            console.error(error);
+        }
+
+        setProjectCode("");
+        setProjectDescription("");
+        setProjectName("");
+    };
+
+    return (
+        <>
+            {/* {!username && ( */}
+            <section>
+                <h3>Enter Project Details</h3>
+                <form onSubmit={onSubmit} className={styles.enterForm}>
+                    <input placeholder="project code" onChange={onProjectCodeChange} />
+                    <input placeholder="project description" onChange={onProjectDescriptionChange} />
+                    <input placeholder="project name" onChange={onProjectNameChange} />
+                    {/* <UsernameMessage username={projectName} isValid={isValid} loading={loading} /> */}
+                    <button type="submit" className="btn-green">
+                        Create New Project
+                    </button>
+                    {/* <h3>Debug State</h3>
+                    <div>
+                        Username: {userName}
+                        <br />
+                        Loading: {loading.toString()}
+                        <br />
+                        Username Valid: {isValid.toString()}
+                    </div> */}
+                </form>
+            </section>
+            {/* )} */}
         </>
     );
 }
